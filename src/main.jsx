0e7a586139bab67312a40b1a8588751bac40ebaf
Len1na <andreaalonso82garcia@gmail.com>
2025-01-17 20:53:57 +0100

    fix infinity loop on App componet

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
+const parentElement = document.getElementById("root");
+createRoot(parentElement).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
