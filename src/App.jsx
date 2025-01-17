27a79fb2ece565d13d5411959efbe93eca5efc33
Len1na <andreaalonso82garcia@gmail.com>
2025-01-17 21:13:43 +0100

    Remove this unused import of 'viteLogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg'; // Esta línea se puede eliminar
+
+import './App.css';
 
 function App() {
-  
   return (
-    
-      <h1>demo</h1>
-
+    <h1>demo</h1>
   );
 }
 
 export default App;
+
+
