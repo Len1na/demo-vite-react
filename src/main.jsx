97f38a94f716cfd5d9cc2ac98f35b00fd27e2c57
Len1na <andreaalonso82garcia@gmail.com>
2025-01-17 21:22:24 +0100

    remove unused import of useState

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 const parentElement = document.getElementById("root");
 createRoot(parentElement).render(
